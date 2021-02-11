d69f5a972272fbd0e5c1eecda7186a35744e4289
shnyder <ajs.private@gmail.com>
2021-02-11 15:44:21 +0100

    testing linter/prettier in apps-folder

--- apps/main-frontend/src/components/MUIComponent-in-UI-lib.tsx
+++ apps/main-frontend/src/components/MUIComponent-in-UI-lib.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { MUIButton } from '@your-npm-org/ui-lib';
 
 export const MUIComponentInUIlib = () => {
+	//trying to commit something ugly in the apps-folder now:
+	const terrible_Code = 1313 * 1 + 'true';
 	return <MUIButton>this is a Material UI button</MUIButton>;
 };
