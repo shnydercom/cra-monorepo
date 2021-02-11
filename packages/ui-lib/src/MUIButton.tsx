d066dbf283ea569bec9889c6ea18be2fe74d4d8d
shnyder <ajs.private@gmail.com>
2021-02-11 15:40:20 +0100

    added a comment

--- packages/ui-lib/src/MUIButton.tsx
+++ packages/ui-lib/src/MUIButton.tsx
@@ -6,6 +6,6 @@ export interface MUIButtonProps extends ButtonProps {}
 export const MUIButton = ({ ...props }: MUIButtonProps) => {
 	// changing it just to show that the import works:
 	props.centerRipple = true;
-	props['aria-details'] = 'this is some ugly code';
+	props['aria-details'] = 'this is some ugly code'; //try to move a statement like this on separate lines and see what happens before commiting
 	return <Button {...props} />;
 };
